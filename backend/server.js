eb3942fb8561109e3f94912ddf169c6a948e2b35
Miguel Castilleja <castilleja.miguel@gmail.com>
2023-03-30 14:47:25 -0600

    Corregido errores e implementado colors

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const errorHandler = require('./middleware/errorMiddleware')
-const port = process.env.PORT || 5000
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const mongoDBConnexion = require('./config/db')
+const port = process.env.PORT || 5000
 
 mongoDBConnexion()
 
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/product', require('./routes/productsRoutes'))
 app.use('/api/user', require('./routes/usersRoutes'))
-app.use('/api/order', require('./routes'))
-
+app.use('/api/order', require('./routes/ordersRoutes'))
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server start on port ${port}`))
+app.listen(port, () => console.log(`Server start on port ${port}`.blue))
